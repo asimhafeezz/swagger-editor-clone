4eaeb98a127a0964b857acb94ca477bb091e9ab7
asimhyff <asimhyff@users.noreply.github.com>
2020-12-29 17:39:00 +0500

    updated

--- src/components/path.js
+++ src/components/path.js
@@ -6,12 +6,21 @@ const Root = styled.div``
 const HeaderContainer = styled.div`
 	background-color: ${({ heading }) =>
 		heading === 'put'
-			? 'rgb(255, 119, 0, 0.6)'
+			? 'rgb(255, 119, 0, 0.2)'
 			: heading === 'get'
-			? 'rgb(46, 70, 179, 0.6)'
+			? 'rgb(46, 70, 179, 0.2)'
 			: heading === 'patch'
-			? 'rgb(36, 171, 47, 0.6)'
-			: heading === 'delete' && 'rgb(255, 0, 21, 0.6)'};
+			? 'rgb(36, 171, 47, 0.2)'
+			: heading === 'delete' && 'rgb(255, 0, 21, 0.2)'};
+	border: 1px solid
+		${({ heading }) =>
+			heading === 'put'
+				? 'rgb(255, 119, 0)'
+				: heading === 'get'
+				? 'rgb(46, 70, 179)'
+				: heading === 'patch'
+				? 'rgb(36, 171, 47)'
+				: heading === 'delete' && 'rgb(255, 0, 21)'};
 	margin: 0.5rem 0;
 	padding: 0.5rem;
 	max-width: 700px;
@@ -34,7 +43,7 @@ const Header = styled.div`
 		font-weight: 500;
 		padding: 0.2rem;
 		padding: 0.2rem 0.7rem;
-		border-radius: 5px;
+		border-radius: 3px;
 		border: 0;
 		outline: none;
 		text-transform: uppercase;
